0f756921844830935078552ed38455c7efbf15c1
Rafael Toledo <rafaeltol@gmail.com>
2016-12-01 10:20:59 -0200

    dont train the models again, just upload

--- best_models.R
+++ best_models.R
@@ -7,12 +7,15 @@ source("utils.R")
 
 ### QDA - FORMULA 1 - THRESHOLD 1e9
 ### RANDOM FOREST - FORMULA 1 - THRESHOLD 1e7
-train <- get_features( audios[, path], thrs=1e9)
-bm.qda <- qda( eval(parse(text=form.1)), train)
+# train <- get_features( audios[, path], thrs=1e9)
+# bm.qda <- qda( eval(parse(text=form.1)), train)
+# saveRDS(bm.qda, "bm.qda.form1.th1e9.rds")
+bm.qda <- readRDS("./models/bm.qda.form1.th1e9.rds")
 
-train <- get_features( audios[, path], thrs=1e7)
-bm.rf <- randomForest( eval(parse(text=form.1)), train)
-
+# train <- get_features( audios[, path], thrs=1e7)
+# bm.rf <- randomForest( eval(parse(text=form.1)), train)
+# saveRDS(bm.rf, "bm.rf.form1.th1e7.rds")
+bm.rf <- readRDS("./models/bm.rf.form1.th1e7.rds")
 
 predict_qda <- function( test_path, model=bm.qda){
     test_feat <- get_features( test_path, thrs=1e9)
@@ -29,9 +32,10 @@ predict_rf <- function( test_path, model=bm.rf){
     return(new_pred)
 }
 
-testdir_set <-  c("./audit_newrecords_2/cutted_carrier_message/",
-                  "./audit_newrecords_2/cutted_human_answer/",
-                  "./audit_newrecords_2/cutted_unknown/")
+testdir_set <-  c("./data/test/audit_newrecords_2/cutted_carrier_message/",
+                  "./data/test/audit_newrecords_2/cutted_human_answer/",
+                  "./data/test/audit_newrecords_2/cutted_unknown/" )
+                 # "./data/test/audit_newrecords/cutted_answering_machine/") # used to create the first test error analysis
 
 testfiles <- rbindlist( lapply(testdir_set, get_metadata))
 
